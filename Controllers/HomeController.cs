79a545ebea0953d7a2ad04b9bc8ed502928aef7a
FlexPudge <micha.parmuzin@yandex.ru>
2023-05-03 21:29:15 +0300

    Кофиксил структуру проекта

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace Gazenergokomplekt.Controllers
         {
             return View();
         }
+        public IActionResult Work()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
